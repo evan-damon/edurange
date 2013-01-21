8e7cf1afa4e060de762d7b40facd8fecd6e63151
Ubuntu <ubuntu@ip-10-117-94-241.ec2.internal>
2013-01-21 22:36:44 +0000

    Added more things.

--- init.rb
+++ init.rb
@@ -45,12 +45,36 @@ def gen_client_ssl_cert
   puts uuid
   # Create cert for name on puppetmaster
   `sudo puppetca --generate #{uuid}`
-  ssl_cert = `sudo cat /var/lib/puppet/ssl/certs/#{uuid}.pem`
-  ca_cert = `sudo cat /var/lib/puppet/ssl/certs/ca.pem`
-  private_key = `sudo cat /var/lib/puppet/ssl/private_keys/#{uuid}.pem`
+  ssl_cert = `sudo cat /var/lib/puppet/ssl/certs/#{uuid}.pem`.chomp
+  ca_cert = `sudo cat /var/lib/puppet/ssl/certs/ca.pem`.chomp
+  private_key = `sudo cat /var/lib/puppet/ssl/private_keys/#{uuid}.pem`.chomp
   return [uuid, ssl_cert, ca_cert, private_key]
 
   # Ensure client has line in puppet.conf to use generated cert
+end
+
+def generate_puppet_conf(uuid)
+  conf_file = <<conf
+[main]
+logdir=/var/log/puppet
+vardir=/var/lib/puppet
+ssldir=/var/lib/puppet/ssl
+rundir=/var/run/puppet
+factpath=$vardir/lib/facter
+templatedir=$confdir/templates
+prerun_command=/etc/puppet/etckeeper-commit-pre
+postrun_command=/etc/puppet/etckeeper-commit-post
+runinterval=60 # run every minute for debug TODO REMOVE
+
+[master]
+# These are needed when the puppetmaster is run by passenger
+# and can safely be removed if webrick is used.
+ssl_client_header = SSL_CLIENT_S_DN 
+ssl_client_verify_header = SSL_CLIENT_VERIFY
+
+[agent]
+certname=#{uuid}
+conf
 end
 
 #def init_crontab
@@ -58,24 +82,30 @@ end
   # * * * * * ec2-describe-instances | grep ^INSTANCE | grep -v terminated | awk '{print $4}' > /etc/puppet/autosign.conf 
 #end
 
-def write_shell_config_file(ssh_key, puppetmaster_ip, certs)
+def write_shell_config_file(ssh_key, puppetmaster_ip, certs, puppet_conf)
   File.open("my-user-script.sh", 'w') do |file|
     file_contents = <<contents
 #!/bin/sh
 set -e
 set -x
-echo "Hello World.  The time is now $(date -R)!" | tee /root/output.txt' + "
+echo "Hello World.  The time is now $(date -R)!" | tee /root/output.txt
 apt-get update; apt-get upgrade -y
 
 key='#{ssh_key.chomp}'
 echo $key >> /home/ubuntu/.ssh/authorized_keys
 
-echo #{puppetmaster_ip} puppet >> /etc/hosts "
+echo #{puppetmaster_ip} puppet >> /etc/hosts
 apt-get -y install puppet
 
-echo '#{certs[1]}' >> /var/lib/puppet/ssl/certs/#{certs[0]}.pem
-echo '#{certs[2]}' >> /var/lib/puppet/ssl/certs/ca.pem
-echo '#{certs[3]}' >> /var/lib/puppet/ssl/private_keys/#{certs[0]}.pem
+mkdir -p /var/lib/puppet/ssl/certs
+mkdir -p /var/lib/puppet/ssl/private_keys
+mkdir -p /etc/puppet
+
+echo '#{certs[1]}' >> "/var/lib/puppet/ssl/certs/#{certs[0]}.pem"
+echo '#{certs[2]}' >> "/var/lib/puppet/ssl/certs/ca.pem"
+echo '#{certs[3]}' >> "/var/lib/puppet/ssl/private_keys/#{certs[0]}.pem"
+
+echo '#{puppet_conf.chomp}' > /etc/puppet/puppet.conf
 
 sed -i /etc/default/puppet -e 's/START=no/START=yes/'
 service puppet restart
@@ -90,6 +120,7 @@ end
 our_ssh_key = get_our_ssh_key()
 
 certs = gen_client_ssl_cert()
-write_shell_config_file(our_ssh_key,puppetmaster_ip, certs)
+conf = generate_puppet_conf(certs[0])
+write_shell_config_file(our_ssh_key,puppetmaster_ip, certs, conf)
 
-#run(command)
+run(command)
